1aa3a5cd252bc886b336168c07193be7425cf622
MrCreeperH <olgaakshidi.ru@gmail.com>
2022-05-10 14:37:21 +0300

    add rarity and fireproof

--- src/main/java/net/creeper/soc/item/ModItems.java
+++ src/main/java/net/creeper/soc/item/ModItems.java
@@ -11,6 +11,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.*;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
@@ -32,31 +33,31 @@ public class ModItems {
                     new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER).maxCount(64)));
     /////Материалы для брони
     public static final Item CREEPERITE_INGOT = registerItem("creeperite_ingot",
-            new Item(new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER).maxCount(64)));
+            new Item(new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER).maxCount(64).rarity(Rarity.EPIC).fireproof()));
 
     public static final Item CREEPERITE_RAW = registerItem("creeperite_raw",
-            new Item(new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER).maxCount(64)));
+            new Item(new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER).maxCount(64).rarity(Rarity.RARE)));
 
 
     /////Вещи из криперита
     public static final Item CREEPERITE_NUGGET = registerItem("creeperite_nugget",
-            new Item(new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER).maxCount(64)));
+            new Item(new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER).maxCount(64).fireproof()));
     public static final Item CREEPERITE_FRAGMENT = registerItem("creeperite_fragment",
-            new Item(new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER).maxCount(64)));
+            new Item(new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER).maxCount(64).fireproof()));
 
     /////Броня
     public static final Item CREEPERITE_HELMET = registerItem("creeperite_helmet",
             new ArmorItem(ModArmorMaterials.CREEPERITE, EquipmentSlot.HEAD,
-                    new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER)));
+                    new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER).fireproof()));
     public static final Item CREEPERITE_CHESTPLATE = registerItem("creeperite_chestplate",
             new ArmorItem(ModArmorMaterials.CREEPERITE, EquipmentSlot.CHEST,
-                    new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER)));
+                    new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER).fireproof()));
     public static final Item CREEPERITE_LEGGINGS = registerItem("creeperite_leggings",
             new ArmorItem(ModArmorMaterials.CREEPERITE, EquipmentSlot.LEGS,
-                    new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER)));
+                    new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER).fireproof()));
     public static final Item CREEPERITE_BOOTS = registerItem("creeperite_boots",
             new ArmorItem(ModArmorMaterials.CREEPERITE, EquipmentSlot.FEET,
-                    new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER)));
+                    new FabricItemSettings().group(ModItemGroup.STORYOFCREEPER).fireproof()));
 
 
     /////Предметы
@@ -70,11 +71,11 @@ public class ModItems {
     /////Мем-предметы
     public static final Item WHAT = registerItem("what",
             new ModPoisoningSwordItem(ModToolMaterials.WHAT, 50, 50f,
-                    new FabricItemSettings().group(ModItemGroup.NEWSECRETTHINGS)));
+                    new FabricItemSettings().group(ModItemGroup.NEWSECRETTHINGS).rarity(Rarity.EPIC).fireproof()));
 
     public static final Item MORGEN_MUSIC_DISK = registerItem("morgen_music_disk",
             new ModMusicDiscItem(7, ModSounds.MORGEN_MUSIC,
-                    new FabricItemSettings().group(ModItemGroup.NEWSECRETTHINGS).maxCount(1)));
+                    new FabricItemSettings().group(ModItemGroup.NEWSECRETTHINGS).maxCount(1).rarity(Rarity.EPIC).fireproof()));
 
 
 
